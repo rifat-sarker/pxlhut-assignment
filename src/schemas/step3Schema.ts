09f4453fad3325827e97e1f4d99caf1721da1509
rifat-sarker <rifatswd@gmail.com>
2025-04-13 22:59:17 +0600

    ensured password and confirm password are same

--- src/schemas/step3Schema.ts
+++ src/schemas/step3Schema.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+<<<<<<< HEAD
 
 export const step3Schema = z.object({
   username: z.string().min(4, "Username must be at least 4 characters"),
@@ -7,3 +8,18 @@ export const step3Schema = z.object({
     .string()
     .min(6, "Confirm password must be at least 6 characters"),
 });
+=======
+export const step3Schema = z
+  .object({
+    username: z.string().min(4, "Username must be at least 4 characters"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+>>>>>>> c9e1177 (ensured password and confirm password are same)
